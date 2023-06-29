d630eecc74fb0e885bb0f6bf628dba9623d98b13
ixmael <hola@irm.mx>
2023-06-28 20:40:30 -0600

    feat: inital commit

---
+++ core/src/main/java/mx/irm/core/User.kt
@@ -0,0 +1,5 @@
+package mx.irm.core
+
+const val USER_KEY: String = "this is the user key"
+
+class User()
---
+++ distribution/src/main/java/mx/irm/distribution/Distribution.kt
@@ -0,0 +1,3 @@
+package mx.irm.distribution
+
+const val DISTRIBUTION_KEY = "distribution_key"
