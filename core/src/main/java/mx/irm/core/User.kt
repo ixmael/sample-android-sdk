b23fbf42cb29d95af3e991fbb1e3f94d8660d1c1
ixmael <hola@irm.mx>
2023-06-29 21:16:42 -0600

    build: update

--- core/src/main/java/mx/irm/core/User.kt
+++ core/src/main/java/mx/irm/core/User.kt
@@ -1,5 +1,12 @@
 package mx.irm.core
 
+import com.fingerprintjs.android.fpjs_pro.Configuration
+
 const val USER_KEY: String = "this is the user key"
+
+fun Probando() {
+    val config = Configuration("my-api-key")
+    println("ixmael probando ${config}")
+}
 
 class User()
